ea8d1b471e056cac2a1234051b8de3f26fa84881
alheym <alheym.mw@gmail.com>
2025-06-16 13:31:55 +0300

    heart

--- src/pages/main/components/calendar/index.tsx
+++ src/pages/main/components/calendar/index.tsx
@@ -8,30 +8,34 @@ const CalendarSection = () => {
   return (
     <S.Wrapper>
       <S.Title>Когда?</S.Title>
-      <div style={{ position: 'relative'}}>
+        <div style={{position: 'relative'}}>
 
-        <img src={Calendar} alt="Calendar" style={{ width: '100%', height: '100%' }} />
+            <img src={Calendar} alt="Calendar" style={{width: '100%', height: '100%'}}/>
 
-        <motion.div
-          initial={{ scale: 1 }}
-          animate={{ scale: [1, 1.1, 1] }}
-          transition={{
-            duration: 2,
-            ease: 'easeInOut',
-            repeat: Infinity,
-          }}
-          style={{
-            position: 'absolute',
-            left: '19%',
-            bottom: '5%',
-            transform: 'translate(-50%, 0)',
-            width: '15%',
-            height: 'auto',
-          }}
-        >
-          <img src={HeartWithDate} alt="Heart with Date" style={{ width: '100%', height: '100%' }} />
-        </motion.div>
-      </div>
+            <motion.div
+                initial={{ scale: 1, rotate: 0 }}
+                animate={{
+                    scale: [1, 1.1, 1],
+                    rotate: [0, 2, -2, 0]
+                }}
+                transition={{
+                    duration: 1.5,
+                    ease: 'easeInOut',
+                    repeat: Infinity,
+                    repeatType: 'loop'
+                }}
+                style={{
+                    position: 'absolute',
+                    left: '27%',
+                    bottom: '15%',
+                    transform: 'translate(-50%, 0)',
+                    width: '12%',
+                    height: 'auto',
+                }}
+            >
+                <img src={HeartWithDate} alt="Heart with Date" style={{width: '100%', height: '100%'}}/>
+            </motion.div>
+        </div>
     </S.Wrapper>
   );
 };
